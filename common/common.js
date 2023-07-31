55903754d8de2633faece536d29ec8a8f9d3ebff
Breezyafternunes <121128857+Breezyafternunes@users.noreply.github.com>
2023-07-31 09:24:26 -0700

    Update common.js

--- common/common.js
+++ common/common.js
@@ -1,46 +1,53 @@
 <script>
-  // Button Click Event Handler
-  function handleRateButtonClick(postID, topicID) {
-    // Show the pop-up dialog when the button is clicked
-    $("#ratingDialog").show();
+// Button Click Event Handler
+$(".rate-button").click(function () {
+  const postID = $(this).closest(".topic-body").data("post-id");
+  const topicID = $(this).closest(".topic-body").data("topic-id");
+  handleRateButtonClick(postID, topicID);
+});
 
-    // Handle the rating stars interaction
-    $(".rating-stars .star").hover(
-      function () {
-        $(this).prevAll(".star").addClass("active");
-        $(this).addClass("active");
+// Pop-up Handling Functions
+function handleRateButtonClick(postID, topicID) {
+  // Show the pop-up dialog when the button is clicked
+  $("#ratingDialog").show();
+
+  // Handle the rating stars interaction
+  $(".rating-stars .star").hover(
+    function () {
+      $(this).prevAll(".star").addClass("active");
+      $(this).addClass("active");
+    },
+    function () {
+      $(this).prevAll(".star").removeClass("active");
+      $(this).removeClass("active");
+    }
+  );
+
+  // Handle the submit button click
+  $(".submit-button").click(function () {
+    // Get the selected rating value
+    const ratingValue = $(".rating-stars .star.active").data("rating");
+
+    // Hide the pop-up dialog
+    $("#ratingDialog").hide();
+
+    // Pass the ratingValue and topicID to the function for saving the rating
+    saveRating(postID, topicID, ratingValue);
+  });
+}
+
+function saveRating(postID, topicID, ratingValue) {
+  // Use the Discourse API to save the rating as text for the post
+  const ratingField = "rating";
+  api.ajax(`/posts/${postID}/custom_fields`, {
+    type: "PUT",
+    data: {
+      custom_fields: {
+        [ratingField]: ratingValue.toString(),
       },
-      function () {
-        $(this).prevAll(".star").removeClass("active");
-        $(this).removeClass("active");
-      }
-    );
-
-    // Handle the submit button click
-    $(".submit-button").click(function () {
-      // Get the selected rating value
-      const ratingValue = $(".rating-stars .star.active").data("rating");
-
-      // Hide the pop-up dialog
-      $("#ratingDialog").hide();
-
-      // Pass the ratingValue and topicID to the function for saving the rating
-      saveRating(postID, topicID, ratingValue);
-    });
-  }
-
-  function saveRating(postID, topicID, ratingValue) {
-    // Use the Discourse API to save the rating as text for the post
-    const ratingField = "rating";
-    api.ajax(`/posts/${postID}/custom_fields`, {
-      type: "PUT",
-      data: {
-        custom_fields: {
-          [ratingField]: ratingValue.toString(),
-        },
-      },
-    });
-  }
+    },
+  });
+}
 
   api.onPageChange(() => {
     const categoriesToTarget = [".category-mock-library-of-catechesis", ".category-library"];
